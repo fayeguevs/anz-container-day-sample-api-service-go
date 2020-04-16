bc6f4c6c332a319d6536f5702c4017b168f86e8d
Faye Guevarra <49783353+fayeguevs@users.noreply.github.com>
2020-04-16 12:43:37 +1200

    change message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Heyo what's up"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
